a6731969d004faef4a0d6baa35775f22d113c718
vejjutejdeep <tej2544@msitprogram.net>
2020-08-10 20:31:07 +0530

    commit

--- Arrays/triplets.py
+++ Arrays/triplets.py
@@ -3,12 +3,21 @@ for i in range(ntest):
     l = int(input("enter the lenght of the array: "))
     nstr = input("enter the string in single line: ").split()
     arr = list(map(int, nstr))
+    arr = sorted(arr)
     # print(arr)
     cou = 0
-    for el2 in range(len(arr) - 1):
-        for ele in range(el2 + 1, len(arr)):
-            if arr[ele] + arr[el2] in arr:
-                cou += 1
+    p1 = 0
+    p2 = len(arr) - 2
+    while p1 <= len(arr) - 1 and p2 >= 0:
+        print(str(p1) + " pointer1 " + str(p2) + " pointer2 ")
+        if (arr[p1] + arr[p2]) == arr[p2 + 1]:
+            p1 += 1
+            p2 -= 1
+            cou += 1
+        elif (arr[p1] + arr[p2]) > arr[p2 + 1]:
+            p2 -= 1
+        else:
+            p1 += 1
     if cou == 0:
         print(-1)
     else:
