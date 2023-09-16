ebf72ed09f7c55fc573d00f9dc610c766d84dbb4
vejjutejdeep <55695301+vejjutejdeep@users.noreply.github.com>
2023-09-16 12:48:43 +0530

    sorting_changes

---
+++ sorting/selection_sorting.py
@@ -0,0 +1,19 @@
+def sorting(arr):
+
+    print(arr)
+
+    for i in range(len(arr)):
+
+        for j in range(len(arr)):
+
+            if arr[i] <= arr[j]:
+
+                arr[i],arr[j] = arr[j],arr[i]
+    
+    print(arr)
+
+    return arr
+
+arr = [34,1,65,2,67,21]
+
+sorting(arr)
