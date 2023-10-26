27863b914bdb66fb36c6027315fbb40e31a0117a
vejjutejdeep <tej2544@msitprogram.net>
2023-10-26 12:20:09 +0530

    prime number

---
+++ Arrays/prime.py
@@ -0,0 +1,43 @@
+# def prime(num):
+
+#     for i in range(2,num):
+
+#         if num % i == 0:
+
+#             return "not a prime"
+        
+#     return "prime"
+
+# def finacchi_series(num):
+
+#     st_lis = [1,1]
+        
+#     for i in range(2,num):
+
+#         st_lis.append(st_lis[i - 1] + st_lis[i - 2])
+
+#     return st_lis
+def change_string(sentence):
+
+    lis = sentence.split()
+
+    words = len(lis) - 1 
+
+    str = ""
+
+    while(words > -1):
+        # print(words)
+        if "." in lis[words]:
+            lis[words][:-2]
+        str += lis[words] + " "
+
+        words -= 1
+    # print(str)
+
+    return str
+
+a = lambda b,a: a+ b
+
+print(a(2,3))
+
+print(change_string("I am vejju Tejdeep."))
