ce7fd71bc2a1d2a43c9dfb6fef5ff100af8edf69
vejjutejdeep <tej2544@msitprogram.net>
2023-10-30 10:51:51 +0530

    valid Parenthesis

---
+++ DSA/bracket_valid.py
@@ -0,0 +1,53 @@
+def validParantresis(args):
+
+    vaild = {"{" :"}", "[":"]", "(":")"}
+
+    static = []
+
+    for i in args:
+
+        print(i)
+
+        if i in vaild:
+
+            static.append(i)
+        
+        elif i not in vaild and len(static) == 0:
+
+            return False
+        
+        elif (vaild[static[-1]] != i):
+
+            return False
+
+        elif (vaild[static[-1]] == i):
+
+            static.pop(-1)
+        
+        print(static)
+    
+    if len(static) != 0:
+
+        return False
+
+    return True
+    
+    # print(static)
+
+# Input = "()"
+# print(validParantresis(Input))
+
+# Input = "(){}[]"
+# print(validParantresis(Input))
+
+# Input = "(]"
+# print(validParantresis(Input))
+
+# Input = "{[]}"
+# print(validParantresis(Input))
+
+Input = "[([]])"
+print(validParantresis(Input))
+
+
+
