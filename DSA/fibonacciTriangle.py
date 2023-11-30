e4ee10a9a4754cc47f507878a5cbf79d46f4d001
vejjutejdeep <tej2544@msitprogram.net>
2023-11-30 14:56:37 +0530

    construction of fibonacci triangle

---
+++ DSA/fibonacciTriangle.py
@@ -0,0 +1,33 @@
+def fibonacciTriangleGenerator(lines):
+
+    ope1 = 1
+
+    ope2 = 1
+
+    print(ope1)
+
+    numberElement = 1
+
+    for iter1 in range(lines - 1):
+
+        ope1 = 1
+
+        ope2 = 1
+
+        counter = 0
+
+        while(counter <= numberElement):
+            
+            print(str(ope1), end="\t")
+
+            ope1, ope2 = ope2, ope1 +ope2
+
+            counter += 1
+        
+        numberElement = counter
+        
+        print()
+
+print("Enter number of lines to generate:", end=' ')
+lines = int(input())
+fibonacciTriangleGenerator(lines)
