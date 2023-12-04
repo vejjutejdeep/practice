32330737dd71b8f0abfeba63f9e98364d6a4d644
vejjutejdeep <tej2544@msitprogram.net>
2023-12-04 11:04:12 +0530

    last change in palindrom

--- DSA/palindrom.py
+++ DSA/palindrom.py
@@ -33,18 +33,40 @@ def is_palindrome(num):
     
     return flag
 
+def Palidrome2(num):
+
+    negitive = False
+
+    stringnum = str(num)
+
+    if stringnum[0] == '-':
+
+        negitive = True
+
+        stringnum = stringnum[1:]
+
+    num = list(map(int, stringnum))
+
+    print(num)
+
 num1 =121
 print("original number %d" % num1)
 print(is_palindrome(num1))
+print(Palidrome2(num1))
+
 num2 = -121
 print("original number %d" % num2)
 print(is_palindrome(num2))
+print(Palidrome2(num2))
+
 num3 = 10
 print("original number %d" % num3)
 print(is_palindrome(num3))
+
 num4 = 101
 print("original number %d" % num4)
 print(is_palindrome(num4))
+
 num5 = 11003
 print("original number %d" % num5)
 print(is_palindrome(num5))
