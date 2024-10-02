09e73d6fc7d757a17100dfd69582b43525720cf5
vejjutejdeep <tej2544@msitprogram.net>
2024-10-02 10:23:11 +0530

    changes in the 2 sum

--- DSA/two_sum.py
+++ DSA/two_sum.py
@@ -20,18 +20,36 @@ def find_target(arr, t):
                         return resu
 
 
+def find_target_dic(arr,t):
+    map = {}
+    res = []
+    for i in range(len(arr)):
+        ele2 = t - arr[i]
+        if ele2 in map:
+            break
+        else:
+            map[arr[i]] = i
+    res.append(map[ele2])
+    res.append(i)
+    return res
+
+
 arr = [2,7,11,15]
 target = 9
-print("result "  + str(find_target(arr, target)))
+print("result "  , str(find_target(arr, target)))
+print("result "  , str(find_target_dic(arr, target)))
 
 arr2 = [3,3]
 target2 = 6
 print("result "  + str(find_target(arr2, target2)))
+print("result "  + str(find_target_dic(arr2, target2)))
 
 arr3 = [0,3,1,5,7]
 target3 = 6
 print("result "  + str(find_target(arr3, target3)))
+print("result "  + str(find_target_dic(arr3, target3)))
 
 arr4 = [3,2,4]
 target4 = 6
 print("result " + str(find_target(arr4, target4)))
+print("result " + str(find_target_dic(arr4, target4)))
